655c735317b2fb89ca490256af69ca7bdcc06771
Nick Vujicic <nick.vujicic@abc.xyz>
2023-07-13 16:41:29 +0700

    why chose us

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import About from '$lib/components/about/About.svelte';
   import Home from '$lib/components/home/Home.svelte';
+  import WhyChooseUs from '$lib/components/why-choose-us/WhyChooseUs.svelte';
 </script>
 
 <Home />
 <About />
+<WhyChooseUs />
